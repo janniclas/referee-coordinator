a47501fd80a8c9e2e6c44b0c08e4f9bf04c1ddd2
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-09-27 14:05:29 +0200

    moved dockerfile

--- scripts/pushImage.sh
+++ scripts/pushImage.sh
@@ -1,4 +1,4 @@
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_TOKEN"
+docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_TOKEN"
 
 docker tag app docker.pkg.github.com/janniclas/referee-coordinator/app:1.0
 
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,3 +1,2 @@
-cd ..
 docker build -t my-test -f Dockerfile.test .
 docker run my-test
