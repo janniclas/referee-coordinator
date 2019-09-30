d7cc17b4ecbe1af72bbb30ff9d52b74193dc9412
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-09-30 08:46:43 +0200

    explicitly telling docker to run with ci mode to true

--- pushImage.sh
+++ pushImage.sh
@@ -1,3 +1,4 @@
+
 docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_TOKEN"
 
 docker tag app docker.pkg.github.com/janniclas/referee-coordinator/app:1.0
--- runTests.sh
+++ runTests.sh
@@ -1,2 +1,2 @@
 docker build -t my-test -f Dockerfile.test .
-docker run my-test
+docker run -e CI=true my-test npm run test
