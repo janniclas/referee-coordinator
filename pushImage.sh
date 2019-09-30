c29069e46c5f92854bcbe4c1c57603d7b73fe982
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-09-30 11:14:14 +0200

    corrected docker registry adress

--- pushImage.sh
+++ pushImage.sh
@@ -1,6 +1,6 @@
 
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_TOKEN" | docker login docker.pkg.github.com  -u "$DOCKER_USERNAME" --password-stdin
 
-docker tag app docker.pkg.github.com/janniclas/referee-coordinator/app:1.0
+docker tag my-test docker.pkg.github.com/janniclas/referee-coordinator/app:1.0
 
 push docker.pkg.github.com/janniclas/referee-coordinator/app:1.0
