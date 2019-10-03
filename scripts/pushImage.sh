5676a045af34166d1b125be37d234805ac9e6cf2
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-10-03 21:13:40 +0200

    updated travis.yml and added dynamic build name

--- scripts/pushImage.sh
+++ scripts/pushImage.sh
@@ -1,6 +1,5 @@
 echo "$DOCKER_TOKEN" | docker login docker.pkg.github.com  -u "$DOCKER_USERNAME" --password-stdin
 
-docker tag my-prod docker.pkg.github.com/janniclas/referee-coordinator/app:2.0
+docker tag my-prod docker.pkg.github.com/janniclas/referee-coordinator/"$TRAVIS_BRANCH":"$TRAVIS_BUILD_NUMBER"
 
-docker push docker.pkg.github.com/janniclas/referee-coordinator/app:2.0
-
+docker push docker.pkg.github.com/janniclas/referee-coordinator/"$TRAVIS_BRANCH":"$TRAVIS_BUILD_NUMBER"
