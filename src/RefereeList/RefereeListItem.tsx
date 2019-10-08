9eba5f6d1cce3be09e5a3546754f10ad02390e41
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2019-10-08 09:01:23 +0200

    removed unused code

--- src/RefereeList/RefereeListItem.tsx
+++ src/RefereeList/RefereeListItem.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import { Referee } from '../store/refereeReducer';
-import { makeStyles, Theme, createStyles, ListItem, ListItemText, ListItemSecondaryAction } from '@material-ui/core';
-import LevelInput from '../RefereeInput/LevelInput';
-import NameInput from '../RefereeInput/NameInput';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }),
-);
+import { ListItem, ListItemText } from '@material-ui/core';
 
 
 const RefereeListItem = (props: {referee: Referee}) => {
-    //const classes = useStyles();
-    console.log('pr', props);
+
     return (
         props.referee ? 
         <ListItem key={props.referee.id}>
