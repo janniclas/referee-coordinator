c4d9ab9669ccfa88e44d1b090ef1d741a26e10e4
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2019-10-16 14:17:17 +0200

    added drop functionality dummy to gametable

--- src/Games/GameTable.tsx
+++ src/Games/GameTable.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { makeStyles, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { Game } from '../types/game';
+import { ObjectState } from '../store/objectReducer';
+import { AppState } from '../store/store';
+import { connect } from 'react-redux';
+import { ObjectType, ObjectTypes } from '../types/types';
+import { useDrop } from 'react-dnd';
 
+const useStyles = makeStyles({
+    root: {
+      width: '100%',
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 650,
+    },
+});
 
-const GameTable = () => {
+const mapStateToProps = (state: AppState) => ({
+    games: state.game
+});
 
-    return (<div></div>);
+const moveObject = (item: ObjectTypes) => {
+    console.log('moved object', item);
+};
+
+const GameTable = (prop: {games: ObjectState<Game>}) => {
+    const classes = useStyles();
+
+    
+    const [, drop] = useDrop<ObjectTypes, any, any>({
+        accept: Object.values(ObjectType),
+        drop: (item) => moveObject(item),
+    });
+
+    return (
+    <div>
+        <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+            <TableRow ref={drop}>
+                <TableCell>Time</TableCell>
+                <TableCell>Home</TableCell>
+                <TableCell>Visitor</TableCell>
+                <TableCell>Location</TableCell>
+                <TableCell>Refs</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {prop.games.objectIds.map(id => (
+                <TableRow key={id}>
+                <TableCell>
+                    {prop.games.objects[id].time}
+                </TableCell>
+                <TableCell >{prop.games.objects[id].home}</TableCell>
+                <TableCell >{prop.games.objects[id].visitor}</TableCell>
+                <TableCell >{prop.games.objects[id].location}</TableCell>
+                <TableCell >{prop.games.objects[id].refs}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </Paper>
+    </div>
+    );
 }
 
-export default GameTable;
+export default connect(mapStateToProps)(GameTable);
--- src/types/game.ts
+++ src/types/game.ts
@@ -1,5 +1,6 @@
 import { MetaInfo } from "./types";
 import { Team } from "./team";
+import { Referee } from "./referee";
 
 export interface GameState {
   games: {[id: string] : Game},
@@ -7,8 +8,9 @@ export interface GameState {
 }
 
 export interface Game extends MetaInfo {
-    home: Team,
-    visitor: Team,
-    location: string,
-    time: number
+    home?: Team,
+    visitor?: Team,
+    location?: string,
+    time?: number,
+    refs?: Array<Referee>
 }
