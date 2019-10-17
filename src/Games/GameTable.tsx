d86f451162b7165419fbf98db18356eaf845370a
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2019-10-17 09:00:21 +0200

    added drag and drop visual handling for table headers

--- src/Games/GameTable.tsx
+++ src/Games/GameTable.tsx
@@ -6,6 +6,7 @@ import { AppState } from '../store/store';
 import { connect } from 'react-redux';
 import { ObjectType, ObjectTypes } from '../types/types';
 import { useDrop } from 'react-dnd';
+import TableHeader from './TableHeader';
 
 const useStyles = makeStyles({
     root: {
@@ -25,13 +26,22 @@ const moveObject = (item: ObjectTypes) => {
     console.log('moved object', item);
 };
 
-const GameTable = (prop: {games: ObjectState<Game>}) => {
+
+const headerCells: Array<{title: string, type?: ObjectType}> = [
+    {title: 'Time'}, 
+    {title: 'Home', type: ObjectType.TEAM},
+    {title: 'Visitor', type: ObjectType.TEAM},
+    {title: 'Location'}, 
+    {title: 'Refs', type: ObjectType.REF}];
+
+const GameTable = (props: {games: ObjectState<Game>}) => {
     const classes = useStyles();
 
     
     const [{ isOver }, drop] = useDrop<ObjectTypes, any, any>({
         accept: Object.values(ObjectType),
         drop: (item) => moveObject(item),
+        canDrop: () => true,
         collect: monitor => ({
 			isOver: !!monitor.isOver(),
 		}),
@@ -42,24 +52,22 @@ const GameTable = (prop: {games: ObjectState<Game>}) => {
         <Paper className={classes.root}>
         <Table className={classes.table} aria-label="simple table">
             <TableHead>
-            <TableRow ref={drop}>
-                <TableCell>Time</TableCell>
-                <TableCell>Home</TableCell>
-                <TableCell>Visitor</TableCell>
-                <TableCell>Location</TableCell>
-                <TableCell>Refs</TableCell>
+            <TableRow>
+                {headerCells.map((header) => (
+                    <TableHeader type={header.type}>{header.title}</TableHeader>
+                ))}
             </TableRow>
             </TableHead>
             <TableBody>
-            {prop.games.objectIds.map(id => (
+            {props.games.objectIds.map(id => (
                 <TableRow key={id}>
                 <TableCell>
-                    {prop.games.objects[id].time}
+                    {props.games.objects[id].time}
                 </TableCell>
-                <TableCell >{prop.games.objects[id].home}</TableCell>
-                <TableCell >{prop.games.objects[id].visitor}</TableCell>
-                <TableCell >{prop.games.objects[id].location}</TableCell>
-                <TableCell >{prop.games.objects[id].refs}</TableCell>
+                <TableCell >{props.games.objects[id].home}</TableCell>
+                <TableCell >{props.games.objects[id].visitor}</TableCell>
+                <TableCell >{props.games.objects[id].location}</TableCell>
+                <TableCell >{props.games.objects[id].refs}</TableCell>
                 </TableRow>
             ))}
             </TableBody>
---
+++ src/Games/TableHeader.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles, TableCell } from '@material-ui/core';
+import { ObjectType, ObjectTypes } from '../types/types';
+import { useDrop } from 'react-dnd';
+
+const useStyles = makeStyles({
+    canDrop: {
+        backgroundColor: 'green',
+    },
+    cantDrop: {
+      backgroundColor: 'red',
+    },
+    normal: {
+        backgroundColor: 'white'
+    }
+});
+
+
+const TableHeader = (props: {type?: ObjectType, children: string}) => {
+    
+    const classes = useStyles();
+
+    const moveObject = (item: ObjectTypes) => {
+        console.log('moved object', item);
+        //TODO: emit dropped event to update / create game depending on drop location and game state
+        // if there is no game create game and add item
+    };
+
+    const canDropObj = (item: ObjectTypes) => {
+        return props.type ? props.type == item.type : false;
+    }
+    
+    const [{ isOver, canDrop }, drop] = useDrop<ObjectTypes, any, any>({
+        accept: Object.values(ObjectType),
+        drop: (item) => moveObject(item),
+        canDrop: (item) => canDropObj(item),
+        collect: monitor => ({
+            isOver: !!monitor.isOver(),
+            canDrop: !!monitor.canDrop(),
+		}),
+    });
+
+
+    const styleToUse = props.type && isOver ? (canDrop ? classes.canDrop : classes.cantDrop): classes.normal
+    return (
+        <TableCell className={styleToUse} ref={drop}>{props.children}</TableCell>
+    );
+}
+
+export default TableHeader;
