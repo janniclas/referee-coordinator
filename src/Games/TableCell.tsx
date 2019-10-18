5864d394ecba5a3f47d8cae7dde10721641cbd7d
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2019-10-18 08:42:17 +0200

    extended table cell to know it's own type

--- src/Games/TableCell.tsx
+++ src/Games/TableCell.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles, TableCell } from '@material-ui/core';
 import { ObjectType, ObjectTypes } from '../types/types';
 import { useDrop } from 'react-dnd';
+import { cellTypes } from './TableRow';
 
 const useStyles = makeStyles({
     canDrop: {
@@ -16,29 +17,34 @@ const useStyles = makeStyles({
 });
 
 
-const MyTableCell = (props: {type?: ObjectType, children?: string, moveObject: (item: ObjectTypes) => void}) => {
+const MyTableCell = (props: {type?: {objType: ObjectType, cellType: cellTypes}, children?: string, moveObject: (item: ObjectTypes, targetCell: cellTypes) => void}) => {
     
     const classes = useStyles();
 
+
     const canDropObj = (item: ObjectTypes) => {
-        return props.type ? props.type === item.type : false;
+        return props.type ? props.type.objType === item.type : false;
     }
-    
+
     const [{ isOver, canDrop }, drop] = useDrop<ObjectTypes, any, any>({
         accept: Object.values(ObjectType),
-        drop: (item) => props.moveObject(item),
+        drop: (item) => { 
+            if (props.type !== undefined) 
+                props.moveObject(item, props.type.cellType)},
         canDrop: (item) => canDropObj(item),
         collect: monitor => ({
             isOver: !!monitor.isOver(),
             canDrop: !!monitor.canDrop(),
-		}),
+        }),
     });
-
 
     const styleToUse = props.type && isOver ? (canDrop ? classes.canDrop : classes.cantDrop): classes.normal
     return (
         <TableCell className={styleToUse} ref={drop}>{props.children ? props.children: ''}</TableCell>
     );
+
+    
+
 }
 
 export default MyTableCell;
